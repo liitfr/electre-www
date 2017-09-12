c3e52c85d38b2f916384d338865a5063144ab0af
Mathias Hoffmann <hoffmann.mathias@gmail.com>
2017-09-12 21:22:11 +0200

    Load a dataset + Check if we can start to process data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,85 @@
+/* global $ */
+
 import TweenLite from 'TweenLite'; // eslint-disable-line
 import AttrPlugin from 'AttrPlugin'; // eslint-disable-line
 import dropdowns from 'ShoelaceDropDowns'; // eslint-disable-line
 import tabs from 'ShoelaceTabs'; // eslint-disable-line
 import electre from 'electre-js';
+import csvtojson from 'csvtojson';
 
-const params = {
-  version: 'EI',
-  inputData: {
-    numberOfCriterias: 4,
-    numberOfAlternatives: 5,
-    criterias: ['g1', 'g2', 'g3', 'g4'],
-    weights: [2, 1, 5, 3],
-    alternatives: ['a1', 'a2', 'a3', 'a4', 'a5'],
-    evaluations: [
-      [150, 1, 20, 3000],
-      [300, 0, 10, 0],
-      [250, 0, 10, 2250],
-      [110, 1, 20, 2800],
-      [120, 1, 50, 1000],
-    ],
-    cThreshold: 0.7,
-    dThreshold: 0.6,
-  },
-};
-
-const calculation = electre.start(params.version, params.inputData);
-
-calculation.then((result) => {
-  console.log(result);
-}, () => {
-
+// Load a dataset
+$('#show-example').on('click', (ev) => {
+  ev.preventDefault();
+  const method = $('#method .checked').attr('id');
+  switch (method) {
+    case 'meth-EI':
+      $('#c-index').val(0.7);
+      $('#d-index').val(0.6);
+      $('#e-matrix').val('criterias, g1, g2, g3, g4\nweights, 2, 1, 5, 3\na1, 150, 1, 20, 3000\na2, 300, 0, 10, 0\na3, 250, 0, 10, 2250\na4, 110, 1, 20, 2800\na5, 120, 1, 50, 1000');
+      break;
+    default:
+      break;
+  }
+  $('#start-process').removeAttr('disabled');
 });
+
+// Check if we can start to process data
+// on any form element, catch events
+$('input, textarea').on('input', () => {
+  const method = $('#method .checked').attr('id');
+  const disabled = $('#start-process').is('[disabled]');
+  let canStart = false;
+  // checks depend on method user wants to use
+  switch (method) {
+    case 'meth-EI':
+      if ($('#c-index').val() && $('#d-index').val() && $('#e-matrix').val()) {
+        canStart = true;
+      }
+      break;
+    default:
+      break;
+  }
+  // toggle `disabled` attr of "process" button
+  if (canStart && disabled) {
+    $('#start-process').removeAttr('disabled');
+  } else if (!canStart && !disabled) {
+    $('#start-process').attr('disabled', true);
+  }
+});
+
+// when user wants to start process
+$('#start-process').on('click', () => {
+  $('#start-process, #processing, #kill-process').toggle();
+  // convert csv to json
+  // & start calculation once its done
+  // numberOfCriterias
+  // numberOfAlternatives
+  // criterias
+  // weights
+  // alternatives
+  // evaluations
+  // cThreshold
+  // dThreshold
+
+  csvtojson({ noheader: true })
+    .fromString('1,2,3\n4,5,6\n7,8,9')
+    .on('csv', (csvRow) => { // this func will be called 3 times
+      console.log(csvRow); // => [1,2,3] , [4,5,6]  , [7,8,9]
+    })
+    .on('done', () => {
+      //parsing finished
+    });
+});
+
+
+//
+// const calculation = electre.start(params.version, params.inputData);
+//
+// calculation.then((result) => {
+//   console.log(result);
+// }, () => {
+//
+// });
 
 // -----------------------------------------------------------------------------
 // Logo
